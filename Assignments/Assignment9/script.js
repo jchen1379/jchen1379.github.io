0cc7c9fbfa4a788c749e9464c2f8a63f21bacf86
Jay Chen <55901916+jchen1379@users.noreply.github.com>
2021-01-22 23:57:50 -0500

    Add files via upload

---
+++ Assignments/Assignment9/script.js
@@ -0,0 +1,31 @@
+const john = {
+ 	firstName: 'John',
+ 	lastName: 'Willams',
+ 	birthYear: 1996,
+ 	job: 'student',
+ 	friends: ['Mike', 'Jack', 'Peter'],
+ 	calAge: function() {
+ 		this.age = 2021 - this.birthYear;
+ 		return this.age
+ 	},
+ 	getFriendSummary: function() {
+ 		return `${this.firstName + ' ' + this.lastName} has ${this.friends.length} friends, and his best friends are called ${this.friends[0]}, ${this.friends[1]}, ${this.friends[2]}`;
+ 	},
+	getLicenseSummary: function(){
+		return `${this.firstName + ' ' + this.lastName} is a ${this.calAge()} years old, and he has ${this.age >= 18 ? 'a' : 'no'} driver license.`;
+	}
+ }
+
+console.log(john.getFriendSummary());
+console.log(john.getLicenseSummary());
+
+
+function openModal(){
+	let modalWindow = document.getElementById("modalWindow");
+	modalWindow.style.display = "flex";
+}
+
+function closeModal(){
+	let modalWindow = document.getElementById("modalWindow");
+	modalWindow.style.display = "none";
+}
